2a26aaa09ddd9b7115fd721f22eb9f3e2717b367
kartik.m <homeSweet123>
2022-08-03 15:49:02 +0530

    Nil returns

--- floats.go
+++ floats.go
@@ -10,23 +10,23 @@ func GetFloats(fileName string) ([]float64, error) {
 	var numbers []float64
 	file, err := os.Open(fileName)
 	if err != nil {
-		return numbers, err
+		return nil, err
 	}
 	//
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		number, err := strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
-			return numbers, err
+			return nil, err
 		}
 		numbers = append(numbers, number)
 	}
 	err = file.Close()
 	if err != nil {
-		return numbers, err
+		return nil, err
 	}
 	if scanner.Err() != nil {
-		return numbers, err
+		return nil, err
 	}
 	return numbers, nil
 }
